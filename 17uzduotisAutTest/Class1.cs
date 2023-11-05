9de164de6e792e73170022c23f5199151061f86a
Jankai <Jankai@DESKTOP-61FU6DP>
2023-11-05 21:49:46 +0100

    17uzduotisAT

---
+++ 17uzduotisAutTest/Class1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17uzduotisAutTest
+{
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            int number1 = 995;
+            double expectedResults = 331;
+            double actualRezults = Class2.Divide(number1);
+
+            Assert.AreEqual(expectedResults, actualRezults);
+
+        }
+    }
+}
---
+++ 17uzduotisAutTest/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17uzduotisAutTest
+{
+    internal class Class2
+    {
+        static public double Divide(double number1)
+        {
+            return Math.Round((double)number1 / 3,3) ;
+        }
+        
+    }
+}
